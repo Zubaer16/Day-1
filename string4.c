f4eea7c932d543aa0d2fbc4ae330cd8d1da57d32
Zubaer16 <48915775+Zubaer16@users.noreply.github.com>
2021-04-01 03:52:41 +0600

    Add files via upload

---
+++ string4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+
+    char string1[100];
+    char string2;
+    char string3='#';
+
+    printf("Enter string : ");
+    gets(string1);
+    printf("Enter the search item : ");
+
+
+    int loop,count,length;
+    count=0;
+    length=0;
+    length=strlen(string1);
+    string2=getchar();
+
+    for(loop=0;loop<length;loop++){
+
+
+
+            if (string1[loop]==string2)
+                string1[loop]=string3;
+
+    }
+
+    printf("The occurrence replacement : ");
+    puts(string1);
+    return 0;
+
+
+}
